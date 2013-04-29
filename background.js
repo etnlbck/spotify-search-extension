f591fc90ea8a03f7e42e6b7ea4e6286caddf7f5b
Nate Lubeck <nate@egood.com>
2013-04-28 18:41:50 -0700

    selecting artist now plays most popular track

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest){
     type: 'GET',
     async: true,
     success: function(result){
-           
+           // console.log(result);
       var artists = result['artists'];
       for (var i = 0; i < artists.length && i < 5; i += 1){  
         var artist = artists[i];
@@ -50,6 +50,42 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest){
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(function(text) {
     var obj = memory[text.replace(/ /g, "-")];
-    chrome.tabs.update({ url: obj.url });
+      
+      $.ajax({url:'http://ws.spotify.com/search/1/track?q='+obj.artist,
+        dataType:'json',
+        type:'GET',
+        async:true,
+        success:function(result){
+          
+          var finalResult = [],
+          tracks = result.tracks;
+          tracks.sort(function(a, b){
+              if (a.popularity > b.popularity){
+                return -1;
+              } else {
+                return 1;
+              }
+             return 1; 
+            
+            });
+         
+          
+          $.each(tracks, function(i, k){
+            var popularity = k.popularity;
+            var href = k.href.split(':');
+            
+            finalResult.push({popularity:popularity, url:href[2]});
+
+          });
+
+          var first_url = 'https://play.spotify.com/track/'+finalResult[0].url;
+          console.log(first_url);
+          // chrome.omnibox.onInputEntered.addListener(function(text, suggest){
+
+          //   suggest()
+          // });
+        
+          chrome.tabs.update({ url: first_url });
+        }});
     //loop through results find selected then push to local storage
 });
